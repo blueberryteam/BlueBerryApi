2483ef73703e76886b2c1343483fbae95a8badad
blueberryteam <iranblueberryteam@gmail.com>
2017-07-16 20:20:12 +0430

    BlueBerry

--- bot/BlueBerry.lua
+++ bot/BlueBerry.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-bot_token = "415695896:AAEBaWqM1Lw6SVSHQ-oksewXPFZ3ZlaTz7k" --Put You Token Here
+bot_token = "Token" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 157059515
+sudo_id = ----
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
